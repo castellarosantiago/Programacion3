2e1956620ddc8eee32ed15179be671db2dc584ab
ppaulig <generalpaulawrk@gmail.com>
2025-04-26 17:41:05 -0300

    ejercicio 4

---
+++ TP2/ejercicio4/eventos.js
@@ -0,0 +1,21 @@
+const lista = document.getElementById("lista");
+const formulario = document.getElementById("formulario");
+const input = document.getElementById("inputt");
+
+// Crea un li con el texto ingresado por el usuario
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+    
+    const inputUsuario = input.value.trim();
+    const nuevoLi = document.createElement("li");
+    nuevoLi.textContent = inputUsuario;
+    lista.appendChild(nuevoLi);
+
+// Marca como completadas las tareas donde el usuario haga click
+    lista.addEventListener("click", (e) => {
+        e.target.classList.add("completado");
+    });
+    
+});
+
+
