84932e8c4240206cb472f93d2ece108b233b47a6
Santiago Castellaro <101475544+castellarosantiago@users.noreply.github.com>
2025-05-22 18:36:33 -0300

    turnos models.js, turnos.entity.js y revision de rutas charladas: van en server.js

---
+++ TP3/src/models/mock/turnos.models.js
@@ -0,0 +1,54 @@
+const Turno = require("./../mock/entities/turnos.entity.js");
+const Config = require("./../../config/config.js");
+const jwt = requite("jsonwebtoken");
+
+class TurnosModel {
+  constructor() {
+    this.data = [];
+    //hardcodear para probar con una instancia de turno despues
+  }
+
+  create(turno) {
+    if (!turno) {
+      throw new Error("No envio ningun dato");
+    } else {
+      return turno;
+    }
+    return new Promise((resolve, reject) => {
+      try {
+        turno.id = this.id;
+        this.id++;
+        const turnoEncontrado = this.data.find(
+          (t) => t.idPaciente === Paciente.id
+        );
+        if (turnoEncontrado === null) {
+          this.data.push(turno);
+        } else {
+          throw new Error("El turno ya se ecnuentra asignado");
+        }
+        resolve(turno);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  delete(id) {
+    new Promise((resolve, reject) => {
+      try {
+        const turnoEncontrado = this.data.find(
+          (t) => t.idPaciente === Paciente.id
+        );
+        if (!turnoEncontrado) {
+          throw new Error("No se puede cancelar un turno no asignado");
+        }
+        const pos = this.data.indexOf(turnoEncontrado);
+        this.data.splice(pos, 1);
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+    
+  }
+}
