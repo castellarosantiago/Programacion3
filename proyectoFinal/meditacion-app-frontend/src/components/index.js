9a43a8a18c17cf356661db95b20495bde03a3212
Santiago Castellaro <101475544+castellarosantiago@users.noreply.github.com>
2025-07-02 02:25:23 -0300

    Delete proyectoFinal directory

--- proyectoFinal/meditacion-app-frontend/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import ContadorMeditador from './Inicio/ContadorMeditador';
-import FondoImagenes from './Inicio/FondoImagenes';
-import Personalizacion from './Inicio/Personalizacion';
-import MeditacionCard from './Inicio/MeditacionCard';
-
-export { ContadorMeditador, FondoImagenes, Personalizacion, MeditacionCard };
