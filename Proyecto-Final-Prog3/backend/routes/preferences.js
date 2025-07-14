d26b2f3d7e23aa5460da8900ab53668e86e4b7b4
castellarosantiago <castellarosantiago@gmail.com>
2025-07-14 15:12:09 +0000

    traemos el back del otro repo de prueba

--- Proyecto-Final-Prog3/backend/routes/preferences.js
+++ Proyecto-Final-Prog3/backend/routes/preferences.js
@@ -1,27 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { Preference, User, BreathSetting } = require('../models');
+const { createPreference, getPreferences, deletePreference } = require("../controllers/preferencesControllers");
 
-router.get('/', async (req, res) => {
-  const user = await User.findOne({ where: { email: 'demo@meditacion.com' } });
-  const prefs = await Preference.findAll({ 
-    where: { UserId: user.id },
-    include: BreathSetting
-  });
-  res.json(prefs);
-});
-
-router.post('/', async (req, res) => {
-  const { title, imageCategory, breathSettingId } = req.body;
-  const user = await User.findOne({ where: { email: 'demo@meditacion.com' } });
-  const newPref = await Preference.create({ title, imageCategory, BreathSettingId: breathSettingId, UserId: user.id });
-  res.status(201).json(newPref);
-});
-
-router.delete('/:id', async (req, res) => {
-  const id = req.params.id;
-  await Preference.destroy({ where: { id } });
-  res.sendStatus(204);
-});
+router.post("/:id_user", createPreference);
+router.get("/:id_user", getPreferences);
+router.delete("/:id_breath", deletePreference);
 
 module.exports = router;
